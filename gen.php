b027fbda915455e67e4f599a74b22768901245f8
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-04-22 00:13:10 +0900

    Update gen.php

--- gen.php
+++ gen.php
@@ -3,6 +3,8 @@ $sourceBaseDir = __DIR__ . '/core/public';
 $distBaseDir   = __DIR__ . '/dist';
 $originalCwd   = getcwd();
 
+$originalIncludePath = get_include_path();
+
 if (!is_dir($distBaseDir)) mkdir($distBaseDir, 0755, true);
 
 $it = new RecursiveDirectoryIterator($sourceBaseDir, RecursiveDirectoryIterator::SKIP_DOTS);
@@ -16,36 +18,29 @@ foreach ($files as $file) {
     
     $relativePath = str_replace($sourceBaseDir, '', $sourcePath);
     $targetHtmlPath = $distBaseDir . $relativePath . '.html';
-    $targetPhpPath  = $distBaseDir . $relativePath;
 
     if (!is_dir(dirname($targetHtmlPath))) mkdir(dirname($targetHtmlPath), 0755, true);
 
     echo "Rendering: {$relativePath} ... ";
 
+    set_include_path($sourceDir . PATH_SEPARATOR . $originalIncludePath);
+    
     chdir($sourceDir);
 
     ob_start();
     try {
         include basename($sourcePath); 
-        $output = ob_get_contents();
+        $output = ob_get_clean();
+        
+        file_put_contents($targetHtmlPath, $output);
+        echo "Saved\n";
     } catch (Throwable $e) {
+        $output = ob_get_clean();
         echo "Error! " . $e->getMessage() . "\n";
-        ob_end_clean();
-        chdir($originalCwd);
-        continue;
     }
-    ob_end_clean();
 
     chdir($originalCwd);
-
-    file_put_contents($targetHtmlPath, $output);
-
-    if (file_exists($targetPhpPath)) {
-        unlink($targetPhpPath);
-        echo "Saved & Deleted .php\n";
-    } else {
-        echo "Saved\n";
-    }
+    set_include_path($originalIncludePath);
 }
 
 echo "\nBuild Finished.\n";
