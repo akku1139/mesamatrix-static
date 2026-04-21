a98a1e201fb980acbe50ae5a77b54d8f0e50c3e0
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-04-21 23:47:33 +0900

    Update gen.php

--- gen.php
+++ gen.php
@@ -1,6 +1,9 @@
 <?php
 $sourceBaseDir = __DIR__ . '/core/public';
 $distBaseDir   = __DIR__ . '/dist';
+$originalCwd   = getcwd();
+
+if (!is_dir($distBaseDir)) mkdir($distBaseDir, 0755, true);
 
 $it = new RecursiveDirectoryIterator($sourceBaseDir, RecursiveDirectoryIterator::SKIP_DOTS);
 $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::LEAVES_ONLY);
@@ -9,27 +12,31 @@ foreach ($files as $file) {
     if ($file->getExtension() !== 'php') continue;
 
     $sourcePath = $file->getRealPath();
-
+    $sourceDir  = dirname($sourcePath);
+    
     $relativePath = str_replace($sourceBaseDir, '', $sourcePath);
+    $targetHtmlPath = $distBaseDir . $relativePath . '.html';
+    $targetPhpPath  = $distBaseDir . $relativePath;
 
-    $targetHtmlPath = $distBaseDir . $relativePath . '.html';	// dist/.../file.php.html
-    $targetPhpPath  = $distBaseDir . $relativePath;		// dist/.../file.php
-
-    $targetDir = dirname($targetHtmlPath);
-    if (!is_dir($targetDir)) mkdir($targetDir, 0755, true);
+    if (!is_dir(dirname($targetHtmlPath))) mkdir(dirname($targetHtmlPath), 0755, true);
 
     echo "Rendering: {$relativePath} ... ";
 
+    chdir($sourceDir);
+
     ob_start();
     try {
-        include $sourcePath;
+        include basename($sourcePath); 
         $output = ob_get_contents();
     } catch (Throwable $e) {
         echo "Error! " . $e->getMessage() . "\n";
         ob_end_clean();
+        chdir($originalCwd);
         continue;
     }
     ob_end_clean();
+
+    chdir($originalCwd);
 
     file_put_contents($targetHtmlPath, $output);
 
